952800f5622ddb4010bf2496bfcbd64c61ae96ef
shohedul1 <shohedulislam564@gmail.com>
2023-09-30 15:16:38 +0600

    frist-commit

---
+++ src/server.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const shortid = require('shortid');
+const fs = require('fs/promises');
+const path = require('path');
+const dbLocation = path.resolve('src', 'data.json');
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+
+
+// GET /products  -      find all avilable product from the system
+//POST /products  -      create new product
+//GET /products/id  -    find a single product
+//put /porducts/id  -    update a single product
+//PATCH /products/id -   update a single product
+//DELETE /products/id -  delete a single product
+
+
+app.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+    let player = players.find((item) => item.id == id);
+
+    if(!player){
+        return res.status(404).json({ message: 'player Not Found'});
+    }
+
+    const newPlayers = players.filter(item => item.id !== id);
+    await fs.writeFile(dbLocation, JSON.stringify(newPlayers));
+    res.status(203).send();
+
+});
+
+app.put('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+    let player = players.find((item)=>item.id == id);
+
+    if(!player){
+        const player = {
+            ...req.body,
+            id: shortid.generate(),
+        };
+        players.push(player);
+    }else{
+        player.name = req.body.name;
+        player.country = req.body.country;
+        player.rank = req.body.rank;
+    }
+
+    await fs.writeFile(dbLocation, JSON.stringify(players));
+    res.status(200).json(player);
+
+});
+
+app.patch('/:id', async (req,res)=>{
+    const id = req.params.id;
+
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+    const player = players.find((item)=>item.id == id);
+
+    if(!player){
+        return res.status(404).json({ message: 'Player Not Found'});
+    }
+
+    player.name = req.body.name || player.name;
+    player.country = req.body.name || player.country;
+    player.rank = req.body.rank || player.rank;
+
+    await fs.writeFile(dbLocation, JSON.stringify(players));
+    res.status(200).json(player);
+
+
+});
+
+app.get('/:id', async (req, res)=>{
+    const id = req.params.id
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+
+    const player = players.find(item => item.id == id);
+
+    if(!player){
+        return res.status(400).json({message: "Player Not Found"});
+    }
+
+    res.set('Cache-control','public, max-age=300');
+    res.status(200).json(player);
+
+
+
+});
+
+app.post('/', async (req,res)=>{
+    const player = {
+        ...req.body,
+        id: shortid.generate(),
+    };
+
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+    players.push(player);
+
+    await fs.writeFile(dbLocation, JSON.stringify(players));
+    res.status(201).json(player);
+
+});
+
+app.get('/', async (req,res)=>{
+    const data = await fs.readFile(dbLocation);
+    const players = JSON.parse(data);
+    res.set('Cache-control','public, max-age=300');
+    res.status(201).json(players);
+});
+
+app.get('/health', (_req, res)=>{
+    res.status(200).json({status:'ok'});
+})
+
+const port = process.env.PORT || 4000;
+app.listen(port, ()=>{
+    console.log(`Server is listening on PORT ${port}`);
+    console.log(`localhost:${port}`);
+})
